707863fbe0cedb35631aa9c2b8d3c6d40907efb7
Zsigmond Kun <42650040+ZsigmondKun@users.noreply.github.com>
2020-11-04 14:05:01 +0100

    Delete use it to create an intelligle dataframe out of the excel file called df_artists_clustered_final.R

--- use it to create an intelligle dataframe out of the excel file called df_artists_clustered_final.R
+++
@@ -1,9 +0,0 @@
-setwd('C:/Users/Administrator/Desktop/Big Data Analytics/project')
-df_final_cluster <- read.csv('df_artists_clustered_final.csv',header = TRUE, sep = ";")
-col_to_be <- c('artists', 'clusters')
-df_final_cluster_artists <- df_final_cluster[,col_to_be] 
-
-df_artists <- read.csv('data_by_artist.csv',header = TRUE, sep = ",")
-df_artists_clustered_new <- left_join(df_artists, df_final_cluster_artists, by = 'artists')
-df_artists_clustered_new <- na.omit(df_artists_clustered_new)
-
